4140c4bf9129c4df97934ef068b4de0422765b71
Harry Bloch <harryfbloch@gmail.com>
2018-09-23 19:08:36 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,7 @@ def apply_clearance(cart)
   clearance_cart
 end
 
-def checkout(cart: [], coupons: [])
+def checkout(cart, coupons)
   total = 0
  consolidated = consolidate_cart(cart: cart)
   coupons_applied = apply_coupons(cart: consolidated, coupons: coupons)
