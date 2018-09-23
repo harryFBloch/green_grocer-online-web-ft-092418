9617a54f1850b4ece9b32584f8a21d4ec0adf5ab
Harry Bloch <harryfbloch@gmail.com>
2018-09-23 18:35:06 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,29 @@
 def consolidate_cart(cart)
   # code here
+  new_hash = {}
+  cart.each { |item_info |
+    item_info.each {|item, item_hash|
+    puts item_hash
+    
+    if !new_hash.has_key?(item)
+      new_hash[item.to_s] = {:price => item_hash[:price], :clearance => item_hash[:clearance], :count => 1}
+    else
+      new_hash[item][:count] += 1 
+    end
+    }
+  }
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coups = {}
+  cart.each {|keys, itemInfo| 
+    coupons.each { |item_hash| 
+      
+      
+    }
+  }
 end
 
 def apply_clearance(cart)
@@ -13,3 +33,11 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+a = [
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"KALE"    => {:price => 3.0, :clearance => false}}
+]
+
+consolidate_cart(a)
